2199e7cbfc80d978b6dc916ace65da762fcb03fa
Toru Makabe <tomakabe@microsoft.com>
2016-01-26 08:10:05 +0900

    Update setup.sh

--- deployLinux/setup.sh
+++ deployLinux/setup.sh
@@ -5,7 +5,7 @@ apt-get -y update
 apt-get -y install apache2 
 
 # write some HTML
-echo \<center\>\<h1\>My Hands Oon Lab Page\</h1\>\<br/\>\</center\> > /var/www/html/index.html
+echo \<center\>\<h1\>My Hands On Lab Page\</h1\>\<br/\>\</center\> > /var/www/html/index.html
 
 # restart Apache
 apachectl restart
